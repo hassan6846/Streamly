296c8be712a2e6cfc9720ee7e02aaa8c70fa82c3
Hassan <96440779+hassan6846@users.noreply.github.com>
2025-02-06 14:04:36 +0500

    Created
    created components and layouts

---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,13 @@
+import "./Alert.css"
+
+const Alert = () => {
+  return (
+    <div className="AlertBar">
+      <p></p>
+      <p className="Alert_text">Ads are shown by the third-party video provider, not us. We appreciate your patience and hope you find our service valuable</p>
+      <p>X</p>
+    </div>
+  )
+}
+
+export default Alert
